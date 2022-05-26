217b12f873c3ee87e18f045a0aa50e4ec057541d
Arquam Ejaz <arquam.ejaz@gmail.com>
2022-05-27 00:54:39 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'regenerator-runtime/runtime'
 import { initContract, login, logout } from './utils'
 
 import getConfig from './config'
-const { networkId } = getConfig(process.env.NODE_ENV || 'development')
+const { networkId } = getConfig('testnet')
 
 import success from './assets/success.png'
 let currentGreeting
